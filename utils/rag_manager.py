a10fbcdb5fbb0d3aac3e2a89be637824128fa5b6
Oscccar1 <2415160142@qq.com>
2026-05-06 20:53:00 +0800

    Fix: Update RecursiveCharacterTextSplitter import path

--- utils/rag_manager.py
+++ utils/rag_manager.py
@@ -2,7 +2,7 @@ import os, tempfile
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.tools import tool
 
 KB_CACHE_DIR = "./knowledge_base_cache"
